7a0b600660936bbbf7606cfb29e3b92ce7adf340
yan41223101 <144768942+yan41223101@users.noreply.github.com>
2023-12-31 10:21:46 +0000

    add 7.

---
+++ downloads/7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int days, years, weeks;
+
+    days = 2000; // Total number of days
+
+    // Converts days to years, weeks and days
+    years = days/365; // Calculate years
+    weeks = (days % 365)/7; // Calculate weeks
+    days = days - ((years*365) + (weeks*7)); // Calculate remaining days
+
+    // Print the results
+    printf("Years: %d\n", years);
+    printf("Weeks: %d\n", weeks);
+    printf("Days: %d \n", days);
+
+    return 0;
+}
