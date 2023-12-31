7d8b411ce7308d484d72d2735d0035ecb022abb3
yan41223101 <144768942+yan41223101@users.noreply.github.com>
2023-12-31 09:29:47 +0000

    add 3.

---
+++ downloads/3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+    // Print a line of hashes
+    printf("######\n");
+
+    // Print a single hash
+    printf("#\n");
+
+    // Print a single hash
+    printf("#\n");
+
+    // Print a line of hashes
+    printf("#####\n");
+
+    // Print a single hash
+    printf("#\n");
+
+    // Print a single hash
+    printf("#\n");
+
+    // Print a single hash
+    printf("#\n");
+
+    return(0);
+}
