0b12986a49cf19d2308768d4f02322915686d9b0
yan41223101 <144768942+yan41223101@users.noreply.github.com>
2023-12-31 08:31:16 +0000

    新增ANSIC

---
+++ downloads/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   :王妍方\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : July 17, 2005\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 0909291966\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
